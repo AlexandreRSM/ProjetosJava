250d55c6b92de03714e0f3ec187c23c6baf2c237
unknown <alexrodriguesmeds@gmail.com>
2020-11-12 16:11:47 -0300

    1.0

---
+++ ProjetoJava1/src/PacoteJava/exjava2.java
@@ -0,0 +1,13 @@
+package PacoteJava;
+
+public class exjava2 {
+	public static void main(String args[])
+	{
+		System.out.println("	Para chegar até aqui foi necessário uma mentalidade de crescimento,"
+				+ "\npara entrar numa área de conhecimento nova, com praticamente zero de experiência."
+				+ "\nTambém foi necessário resiliência, pois não está sendo fácil aprender a linguagem e as ferramentas,"
+				+ "\nmesmo com tanta ajuda do grupo. "
+				+ "\n	A responsabilidade pessoal é um fator importante, preciso estar na melhor situação e condição, para poder aprender e me dedicar 100%"
+				+ "\nmas ainda é algo que não estou fazendo direito.");
+	}
+}
